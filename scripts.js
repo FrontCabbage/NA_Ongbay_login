c26b28849866398449348d9c4f6ce1a6dc12df41
FrontCabbage <126549703+FrontCabbage@users.noreply.github.com>
2023-04-12 00:09:52 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function cusername(){
+    document.getElementById('username').style.backgroundColor = 'gray';
+}
+function rusername(){
+    document.getElementById('username').style.backgroundColor = 'white';
+}
+function cpassword(){
+    document.getElementById('password').style.backgroundColor = 'gray';
+}
+function rpassword(){
+    document.getElementById('password').style.backgroundColor = 'white';
+}
